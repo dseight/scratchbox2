ldbox/ldbox

Make dlopen() function have its own class DLOPEN.
This is needed to allow mapping rules to distinguish between
regular file open operation and dynamic library loading.

Signed-off-by: Oleg Girko <ol@infoserver.lv>

include/mapping.h
@@ -188,6 +188,8 @@ extern char *prep_union_dir(const char *dst_path,
 #define LB_INTERFACE_CLASS_CHOWN	0x20000	/* [fl]?chown, fchownat */
 #define LB_INTERFACE_CLASS_CHMOD	0x40000	/* [fl]?chmod, fchmodat */
 
+#define LB_INTERFACE_CLASS_DLOPEN	0x80000	/* dlopen */
+
 /* interface funtion ->  class(es) mapping table, 
  * created by gen-interface.c */
 typedef struct {

dseight/scratchbox2

include/mapping.h
@@ -185,6 +185,8 @@ extern char *prep_union_dir(const char *dst_path,
 
 #define SB2_INTERFACE_CLASS_CHROOT	0x10000	/* chroot() */
 
+#define SB2_INTERFACE_CLASS_DLOPEN	0x80000	/* dlopen */
+
 /* interface funtion ->  class(es) mapping table, 
  * created by gen-interface.c */
 typedef struct {